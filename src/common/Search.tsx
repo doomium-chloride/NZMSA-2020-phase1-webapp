f850db18ecf0d5df148d6591ca079130465d887d
Jonathan <jsul427@aucklanduni.ac.nz>
2020-07-07 17:34:43 +1200

    Improve Search
    Now the search will check for all words in the query
    Before it returned if it had any word in the query
    Not it only returns if it contains all words in query

--- src/common/Search.tsx
+++ src/common/Search.tsx
@@ -7,49 +7,79 @@ export function search(database: Array<any>, query: string) {
     let output: Array<any> = [];
 
     database.forEach( entry => {
+        let allStrings = [];
         for (var prop in entry) {
             if (Object.prototype.hasOwnProperty.call(entry, prop)) {
                 if(Array.isArray(entry[prop])){
                     let array = entry[prop];
                     let len = array.length;
-                    for(let i = 0; i < len; i++){
-                        if(contains(array[i], query)){
-                            output.push(entry);
-                            return null;//go to next entry
-                        }
-                    }
-                } else if(contains(entry[prop], query)){
-                    output.push(entry);
-                    return null;//go to next entry
+                    allStrings.push(array.join(' '));
+                    // for(let i = 0; i < len; i++){
+                    //     // if(contains(array[i], query)){
+                    //     //     output.push(entry);
+                    //     //     return null;//go to next entry
+                    //     // }
+                    // }
+                } else {
+                    allStrings.push(entry[prop] + "")
+                    // output.push(entry);
+                    // return null;//go to next entry
                 }
             }
+        }
+        const megaString = allStrings.join(' ');
+        if(contains(megaString, query)){
+            output.push(entry);// if the entry mentions all of the query words somewhere: include
         }
     });
 
     return output;
 }
-
-function contains(string1: string, string2: string){
+// string 2 is the query
+// string 1 should contain ALL that is in string2
+export function contains(string1: string, string2: string){
     string1 = string1 + "";
     string2 = string2 + "";
+    string1 = string1.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string2 = string2.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     let arr1 = string1.split(" ");
-    arr1 = arr1.filter(removeEmptyString);
+    arr1 = arr1.filter(removeEmptyString);//super set
     let arr2 = string2.split(" ");
-    arr2 = arr2.filter(removeEmptyString);
+    arr2 = arr2.filter(removeEmptyString);//sub set
 
     const len1 = arr1.length;
     const len2 = arr2.length;
 
+    // for(let i = 0; i < len1; i++){
+    //     for(let j = 0; j < len2; j++){
+    //         if(arr1[i].toLowerCase() == arr2[j].toLowerCase()){
+    //             return true;// one match found
+    //         }
+    //     }
+    // }
     for(let i = 0; i < len1; i++){
-        for(let j = 0; j < len2; j++){
-            if(arr1[i].toLowerCase() == arr2[j].toLowerCase()){
-                return true;// one match found
-            }
-        }
+        arr1[i] = arr1[i].toLowerCase();
     }
-    return false;// no match found
+    for(let i = 0; i < len2; i++){
+        arr2[i] = arr2[i].toLowerCase();
+    }
+
+    return setChecker(arr1, arr2);
+
+    // false = not all queries matched
+    // true = all queries matched
 }
 
 function removeEmptyString(word: string){
     return word.length > 0;
+}
+
+function setChecker(superArr: string[], subArr: string[]){
+    const superSet = new Set(superArr);
+    for(let word of subArr){
+        if(!superSet.has(word)){
+            return false;
+        }
+    }
+    return true;
 }
--- src/test/Search.test.tsx
+++ src/test/Search.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import {search} from '../common/Search';
+import {search, contains} from '../common/Search';
+import { addListener } from 'cluster';
 
 const database = [
     {"artist":"Slawomir Maniak","attack":2,"cardClass":"NEUTRAL","collectible":true,"cost":4,"dbfId":2569,"flavor":"A result of magical experiments carried out by the Black Dragonflight, it's not his fault that he's a vicious killer.","health":6,"id":"AT_017","mechanics":["BATTLECRY"],"name":"Twilight Guardian","race":"DRAGON","rarity":"EPIC","referencedTags":["TAUNT"],"set":"TGT","text":"<b>Battlecry:</b> If you're holding a Dragon, gain +1 Attack and <b>Taunt</b>.","type":"MINION"},
@@ -8,6 +9,10 @@ const database = [
     {"artist":"Tooth","cardClass":"MAGE","collectible":true,"cost":3,"dbfId":2541,"flavor":"Burning man, brah.","id":"AT_002","mechanics":["SECRET"],"name":"Effigy","rarity":"RARE","set":"TGT","text":"<b>Secret:</b> When a friendly minion dies, summon a random minion with the same Cost.","type":"SPELL"},
     {"artist":"Arthur Bozonnet","attack":3,"cardClass":"MAGE","collectible":true,"cost":2,"dbfId":2545,"flavor":"And he can't get up.","health":2,"id":"AT_003","mechanics":["HEROPOWER_DAMAGE"],"name":"Fallen Hero","rarity":"RARE","set":"TGT","text":"Your Hero Power deals 1 extra damage.","type":"MINION"}
 ]
+
+const superString = "Bob the builder can we fix it";
+const subString = "builder fix";
+const wrongSubString = "the darkness";
 
 test('search NEUTRAL', () => {
     const result = search(database, "NEUTRAL");
@@ -24,4 +29,26 @@ test('search burning', () => {
 test('return nothing', () => {
     const result = search(database, "penis");
     expect(result.length).toBe(0);
+});
+
+test('search 2 same', () => {
+    const result = search(database, "burning man");
+    const card = result[0];
+    expect(card.dbfId).toBe(2541);
+});
+
+test('search 2 different', () => {
+    const result = search(database, "burning MAGE mage");
+    const card = result[0];
+    expect(card.dbfId).toBe(2541);
+});
+
+test("contains 2 words", () => {
+    const result = contains(superString, subString);
+    expect(result).toBeTruthy();
+});
+
+test("contains 1 word", () => {
+    const result = contains(superString, wrongSubString);
+    expect(result).toBeFalsy();
 });
